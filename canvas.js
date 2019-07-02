866d222e6bd85352ccb1d7779237642f2378c12e
Romuald Soultan <romuald1.soultan@epitech.eu>
2019-07-02 18:54:52 +0200

    CircleArray implementation

--- canvas.js
+++ canvas.js
@@ -1,24 +1,34 @@
 class Circle {
-    constructor(x, y, radius, fillStyle) {
+    constructor(x, y, radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.fillStyle = fillStyle;
     }
     drawCircle(context) {
         context.beginPath();
         context.lineWidth = "5";
-        context.strokeStyle = this.fillStyle;
+        context.strokeStyle = "#4C8"
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         context.stroke();
     }
 }
 var canvas = document.getElementById("my_canvas");
 var context = canvas.getContext("2d");
-var CircleA = new Circle(50, 50, 50, "#4C8");
-var CircleB = new Circle(400, 550, 50, "#A4A");
-var CircleC = new Circle(750, 50, 50, "#48C");
+var CircleArray = InitCircleArray();
 
-CircleA.drawCircle(context);
-CircleB.drawCircle(context);
-CircleC.drawCircle(context);
+function InitCircleArray() {
+    var CircleArray = [];
+
+    CircleArray.push(new Circle(50, 50, 50));
+    CircleArray.push(new Circle(400, 550, 50));
+    CircleArray.push(new Circle(750, 50, 50));
+    return (CircleArray);
+}
+
+function Draw(CircleArray) {
+    for (var index = 0; index < CircleArray.length; index++) {
+        CircleArray[index].drawCircle(context);
+    }
+}
+
+Draw(CircleArray);
